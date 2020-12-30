9d9d96f4809676051146191d0c5116f4d39dca14
Ali <ali.sheikh@gmx.at>
2020-12-30 22:40:22 +0100

    * removed default view sample.fxml * added views created by Predrag Prclovic: - chat.fxml - login.fxml - settings.fxml
    * added assets created by Predrag Prclovic:
    - avatar.png
    - logo.png

--- src/main/java/at/ac/fhcampuswien/Main.java
+++ src/main/java/at/ac/fhcampuswien/Main.java
@@ -9,14 +9,12 @@ import javafx.stage.Stage;
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("/sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/login.fxml"));
+        primaryStage.setTitle("FAPChat");
+        primaryStage.setScene(new Scene(root, 400, 600));
         primaryStage.show();
-        //// TODO: 22/11/2020 design mockups 
     }
-
 
     public static void main(String[] args) {
         launch(args);
