1c658c4e25f9a42bbd9eb2956706595936ca53ba
Flow <flo-m@gmx.at>
2021-01-27 11:02:06 +0100

    Settings Window Bug with ChatClient fixed

--- src/main/java/at/ac/fhcampuswien/viewmodel/ChatController.java
+++ src/main/java/at/ac/fhcampuswien/viewmodel/ChatController.java
@@ -14,6 +14,7 @@ import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.text.Text;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -66,6 +67,8 @@ public class ChatController {
             Stage stage = new Stage();
             stage.setTitle("Settings");
             stage.setScene(new Scene(root, 390, 600));
+            stage.initOwner(settingsButton.getScene().getWindow());
+            stage.initModality(Modality.WINDOW_MODAL);
             stage.show();
         } catch (IOException e) {
             e.printStackTrace();
